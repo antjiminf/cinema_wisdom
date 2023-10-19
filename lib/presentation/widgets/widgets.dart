b666c65ac4f4ca9c929e4867ce5589c17d2dc0f6
Your Name <antoniojz1998@gmail.com>
2023-10-19 16:57:46 +0200

    exportation of widgets

---
+++ lib/presentation/widgets/widgets.dart
@@ -0,0 +1,5 @@
+export 'shared/custom_appbar.dart';
+export 'movies/movies_slider.dart';
+export 'shared/custom_bottom_navigation_bar.dart';
+export 'movies/movie_horizontal_list.dart';
+export 'shared/full_screen_loader.dart';
