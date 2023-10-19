648d9b2e8c1851b8bb4fee756548dad2577d98e2
Your Name <antoniojz1998@gmail.com>
2023-10-19 16:55:38 +0200

    horizontal list widget

---
+++ lib/presentation/widgets/movies/movie_horizontal_list.dart
@@ -0,0 +1,161 @@
+import 'package:animate_do/animate_do.dart';
+import 'package:cinema_wisdom/config/helpers/human_formats.dart';
+import 'package:cinema_wisdom/domain/entities/movie.dart';
+import 'package:flutter/material.dart';
+
+class MovieHorizontalList extends StatefulWidget {
+  const MovieHorizontalList({
+    super.key,
+    required this.movies,
+    this.title,
+    this.subtitle,
+    this.loadNextPage,
+  });
+
+  final List<Movie> movies;
+  final String? title;
+  final String? subtitle;
+  final VoidCallback? loadNextPage;
+
+  @override
+  State<MovieHorizontalList> createState() => _MovieHorizontalListState();
+}
+
+class _MovieHorizontalListState extends State<MovieHorizontalList> {
+  final scrollController = ScrollController();
+
+  @override
+  void initState() {
+    super.initState();
+    scrollController.addListener(() {
+      if (widget.loadNextPage == null) return;
+      if (scrollController.position.pixels + 200 >=
+          scrollController.position.maxScrollExtent) {
+        widget.loadNextPage!();
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    scrollController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 350,
+      child: Column(
+        children: [
+          if (widget.title != null || widget.subtitle != null)
+            _Title(widget.title, widget.subtitle),
+          Expanded(
+            child: ListView.builder(
+              controller: scrollController,
+              itemCount: widget.movies.length,
+              scrollDirection: Axis.horizontal,
+              physics: const BouncingScrollPhysics(),
+              itemBuilder: (context, index) {
+                return FadeInRight(child: _Slide(movie: widget.movies[index]));
+              },
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class _Title extends StatelessWidget {
+  const _Title(this.title, this.subtitle);
+
+  final String? title;
+  final String? subtitle;
+
+  @override
+  Widget build(BuildContext context) {
+    final themes = Theme.of(context).textTheme;
+    return Container(
+      padding: const EdgeInsets.only(top: 10),
+      margin: const EdgeInsets.symmetric(horizontal: 10),
+      child: Row(
+        children: [
+          if (title != null) Text(title!, style: themes.titleLarge),
+          const Spacer(),
+          if (subtitle != null)
+            FilledButton.tonal(
+              onPressed: () {},
+              style: const ButtonStyle(visualDensity: VisualDensity.compact),
+              child: Text(subtitle!),
+            ),
+        ],
+      ),
+    );
+  }
+}
+
+class _Slide extends StatelessWidget {
+  const _Slide({required this.movie});
+
+  final Movie movie;
+
+  @override
+  Widget build(BuildContext context) {
+    final textStyles = Theme.of(context).textTheme;
+
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 10),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          SizedBox(
+            width: 150,
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(20),
+              child: Image.network(
+                movie.posterPath,
+                width: 150,
+                loadingBuilder: (context, child, loadingProgress) {
+                  if (loadingProgress != null) {
+                    return const CircularProgressIndicator(strokeWidth: 2);
+                  }
+                  return FadeIn(child: child);
+                },
+              ),
+            ),
+          ),
+          const SizedBox(height: 5),
+          SizedBox(
+            width: 150,
+            child: Text(
+              movie.title,
+              maxLines: 2,
+              style: textStyles.titleSmall,
+            ),
+          ),
+          Row(
+            children: [
+              Icon(
+                Icons.star_half_outlined,
+                color: Colors.yellow.shade700,
+              ),
+              const SizedBox(width: 5),
+              Text(
+                '${movie.voteAverage}',
+                style: textStyles.bodyMedium
+                    ?.copyWith(color: Colors.yellow.shade700),
+              ),
+              const SizedBox(width: 10),
+              Text(
+                HumanFormats.number(movie.popularity),
+                // '${movie.popularity}',
+                style: textStyles.bodySmall,
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
