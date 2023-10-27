04ec662610a8de41c5285b76b90a622cb9e14bfe
Your Name <antoniojz1998@gmail.com>
2023-10-27 14:27:33 +0200

    movie details endpoint implementation

--- lib/infrastructure/datasources/moviedb_datasource.dart
+++ lib/infrastructure/datasources/moviedb_datasource.dart
@@ -70,4 +70,15 @@ class MovieDbDatasource extends MovieDatasource {
     final Movie movie = MovieMapper.movieDetailsToEntity(movieDb);
     return movie;
   }
+
+  @override
+  Future<List<Movie>> searchMovies(String query) async {
+    if (query.isEmpty) return [];
+
+    final response = await dio.get(
+      '/search/movie',
+      queryParameters: {'query': query},
+    );
+    return _jsonToMovies(response.data);
+  }
 }
